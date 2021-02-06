059850ee4df726cb8b8cf7764dab44f6a347f638
ooppitz <ooppitz@gmail.com>
2021-02-06 15:34:30 +0100

    Clean up CreateNewStudent

--- SchoolExample/src/main/java/de/azubiag/SchoolExample/app/CreateNewStudent.java
+++ SchoolExample/src/main/java/de/azubiag/SchoolExample/app/CreateNewStudent.java
@@ -1,45 +1,33 @@
 package de.azubiag.SchoolExample.app;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
-
+import de.azubiag.SchoolExample.model.Model;
 import de.azubiag.SchoolExample.model.Student;
-import de.azubiag.SchoolExample.util.Util;
 
 public class CreateNewStudent {
 
-	public static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence
-			.createEntityManagerFactory("schoolDB");
-
 	public static void main(String[] args) {
+		
+		EntityTransaction et = Model.em.getTransaction();
+		et.begin();
 
-		EntityManager em = SchoolDBApp.ENTITY_MANAGER_FACTORY.createEntityManager();
-
-		EntityTransaction et = em.getTransaction();
 		try {
 
-			et.begin();
-
-			// Student w/o an assigned course
-			Student s = new Student("Erich", "Semlak");
-
-			em.persist(s);
-
-			et.commit();
+			// Create Student w/o an assigned course
+			Student s = new Student("Martin", "Kaltenbrunner");
+			Model.em.persist(s);
+			
+			System.out.println("*** Table with new student");
+			Student.printTable();
 
 		} catch (Exception e) {
 
 			e.printStackTrace();
-			if (et != null) {
-				et.rollback();
-			}
-		}
 		
+		}
+		et.rollback();
+		
+		System.out.println("*** Table after rollback");
 		Student.printTable();
 
 	}
