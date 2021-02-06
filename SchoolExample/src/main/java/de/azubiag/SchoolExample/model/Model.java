2826034fa7b0992b81f0a9338c1eb1f7d20ad652
ooppitz <ooppitz@gmail.com>
2021-02-06 13:12:50 +0100

    Add EntityManager to class Model

--- SchoolExample/src/main/java/de/azubiag/SchoolExample/model/Model.java
+++ SchoolExample/src/main/java/de/azubiag/SchoolExample/model/Model.java
@@ -1,5 +1,6 @@
 package de.azubiag.SchoolExample.model;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
@@ -7,5 +8,7 @@ public abstract class Model {
 
 	public static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence
 			.createEntityManagerFactory("schoolDB");
+	
+	public static  EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
 		
 }
